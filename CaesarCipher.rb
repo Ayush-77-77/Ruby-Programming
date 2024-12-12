e6a47dc23eb654926f44241e997220b2a48a6ee9
Ayush-77-77 <ayush95758@gmail.com>
2024-12-12 11:37:47 +0530

    written a program for CaesarCipher that shift till the given position at the right side

---
+++ CaesarCipher.rb
@@ -0,0 +1,45 @@
+# Implemented a Caesar cipher that takes in a string 
+# and the shift factor and then outputs the modified string using a right shift:
+
+def caesar_cipher(string, shift)
+
+  new_str= ""
+  string.each_char do |ch|
+   if ch>='a'&&ch<='z'
+        ascii_value =  smallChar(ch, shift)
+        # new_str += ascii_value.chr
+        print ascii_value.chr
+
+
+     elsif ch>='A'&&ch<='Z'
+      ascii_value =  largeChar(ch, shift)
+    #   new_str += ascii_value.chr
+      print ascii_value.chr
+     else
+      # new_str += ch
+      print ch
+   end
+  end
+
+end
+
+def smallChar(ch, shift)
+  ascii_value = (ch.ord + shift)
+  if (ascii_value > 122 )
+    return 96 + (ascii_value % 122)
+  else
+    return (ascii_value % 123)
+  end
+end
+
+def largeChar(ch, shift)
+  ascii_value = (ch.ord + shift)
+  if (ascii_value > 90)
+    return 64 + (ascii_value % 90)
+  else
+    return (ascii_value % 91)
+  end
+end
+
+
+caesar_cipher("What a string!", 5)
