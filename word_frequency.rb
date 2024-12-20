b95df386a7792dacced90d29578d4484a75abb50
Ayush-77-77 <ayush95758@gmail.com>
2024-12-20 20:58:29 +0530

    add the questions with comments for readability

--- word_frequency.rb
+++ word_frequency.rb
@@ -1,3 +1,9 @@
+# # Write a Ruby method word_frequency that takes an array of strings as input. Each string contains a sentence. The method should:
+# 1. Count the frequency of each word across all the sentences (case-insensitive).
+# 2. Return a hash where keys are the words, and values are their respective frequencies.
+
+
+
 sentences = ["Hello world", "hello Ruby", "Ruby is awesome",  "World is big"]
 new_sentences = sentences.join(' ')
 
